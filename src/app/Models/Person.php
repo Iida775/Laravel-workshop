99db2d8c95b359c3595de1b55fc267d173c38272
iida775 <r.iida@c-innocent.com>
2026-02-17 01:06:17 +0900

    モデルでの新規保存

--- src/app/Models/Person.php
+++ src/app/Models/Person.php
@@ -11,33 +11,16 @@ use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
+    protected $guarded = array('id');//idなどのDBで勝手に降られる値を用意しなくてもエラーにならないようになる
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
+
     public function getData()
     {
-        return $this->id . ': ' . $this->name . ' (' . $this->age . '歳)';
-    }
-
-    //nameが$strと等しいデータを取得するスコープ
-    public static function scopeNameEqual($query, $str)
-    {
-        return $query->where('name', $str);
-    }
-
-    //ageが$n以上のデータを取得するスコープ
-    public static function scopeAgeGreaterThan($query, $n)
-    {
-        return $query->where('age', '>=', $n);
-    }
-
-    //ageが$n以下のデータを取得するスコープ
-    public static function scopeAgeLessThan($query, $n)
-    {
-        return $query->where('age', '<=', $n);
-    }
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::addGlobalScope(new ScopePerson);//これでscopePerson(age>=20)が適用される
+        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
     }
 }
